db283f215c97a625d8626a895d0d6624fe0a0504
jenicaagency <jenicaagency@gmail.com>
2023-01-23 18:20:50 +0200

    scroll navbar

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,1 +1,11 @@
 
+function changeNavcColor (){
+    let navbar = document.querySelector(".custom-nav");
+    let scroll = window.scrollY;
+    if(scroll < 200){
+        navbar.classList.remove("scroll-nav");
+    }else{
+        navbar.classList.add("scroll-nav");
+    }
+}
+window.addEventListener('scroll', changeNavcColor);
