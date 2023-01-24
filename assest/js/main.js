29a8ad7095991123ab64ab687ba8e29e4b741be5
Hagar00 <hagarhassan813@gmail.com>
2023-01-24 15:30:33 +0200

    add cover section

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,11 +1,18 @@
 
 function changeNavcColor (){
     let navbar = document.querySelector(".custom-nav");
+    let logoImg = document.querySelector(".logo-img");
+    console.log(logoImg);
     let scroll = window.scrollY;
-    if(scroll < 200){
+    if(scroll < 30){
         navbar.classList.remove("scroll-nav");
+        logoImg.setAttribute("src","./assest/imgs/logo-black.png");
+      
     }else{
         navbar.classList.add("scroll-nav");
+        logoImg.setAttribute("src","./assest/imgs/logo.png");
     }
 }
 window.addEventListener('scroll', changeNavcColor);
+
+
