11e8a0cb50c6c6dee94285a169828a5d4c4a8f6c
Hagar00 <hagarhassan813@gmail.com>
2023-01-26 05:35:31 +0200

    add section two structure

--- assest/js/main.js
+++ assest/js/main.js
@@ -16,6 +16,27 @@ function changeNavcColor (){
 }
 window.addEventListener('scroll', changeNavcColor);
 
-// animation function on scroll 
+// function tabs in slider two 
+let tabs = document.querySelectorAll(".tabs li");
+let tabsArray = Array.from(tabs);
+let divs = document.querySelectorAll(".content > div");
+let divsArray = Array.from(divs);
+
+// console.log(tabsArray);
+
+tabsArray.forEach((ele) => {
+  ele.addEventListener("click", function (e) {
+    console.log(ele);
+    tabsArray.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+    divsArray.forEach((div) => {
+      div.style.display = "none";
+    });
+    // console.log(e.currentTarget.dataset.cont);
+    document.querySelector(e.currentTarget.dataset.content).style.display = "flex";
+  });
+});
 
 
